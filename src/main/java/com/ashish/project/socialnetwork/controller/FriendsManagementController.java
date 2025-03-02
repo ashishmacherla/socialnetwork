66979de69de0fea5d1df1656484442443586603b
ashishmacharla <102423845+ashishmacharla@users.noreply.github.com>
2025-03-02 11:29:44 -0500

    Prefixed with forward slash before every uri

--- src/main/java/com/ashish/project/socialnetwork/controller/FriendsManagementController.java
+++ src/main/java/com/ashish/project/socialnetwork/controller/FriendsManagementController.java
@@ -18,19 +18,19 @@ public class FriendsManagementController {
     @Autowired
     private FriendsService friendsService;
 
-    @PostMapping("createFriendship/{userId1}/{userId2}")
+    @PostMapping("/createFriendship/{userId1}/{userId2}")
     public ResponseEntity<String> createFriendship(@PathVariable Long userId1, @PathVariable Long userId2) throws FriendshipException {
         friendsService.createFriendship(userId1, userId2);
         return ResponseEntity.accepted().body(String.format("Successfully created friendship between userId1:%s and userId2:%s", userId1, userId2));
     }
 
-    @DeleteMapping("removeFriendship/{userId1}/{userId2}")
+    @DeleteMapping("/removeFriendship/{userId1}/{userId2}")
     public ResponseEntity<String> removeFriendship(@PathVariable Long userId1, @PathVariable Long userId2) {
         friendsService.removeFriendship(userId1, userId2);
         return ResponseEntity.ok().body(String.format("Successfully deleted friendship between userId1:%s and userId2:%s", userId1, userId2));
     }
 
-    @GetMapping("listAllFrienshipsOfUser/id/{userId}")
+    @GetMapping("/listAllFrienshipsOfUser/id/{userId}")
     public ResponseEntity<List<UserDTO>> listFriends(@PathVariable Long userId) {
         return ResponseEntity.ok(friendsService.listFriends(userId));
     }
